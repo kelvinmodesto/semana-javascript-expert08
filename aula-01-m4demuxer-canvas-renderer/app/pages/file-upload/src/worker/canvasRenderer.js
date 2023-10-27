63ff447e0fb33b1b3d4f220945ff2748619fb246
Kelvin Modesto <km.modesto@gmail.com>
2023-10-27 00:23:19 -0300

    Fix canvas

--- aula-01-m4demuxer-canvas-renderer/app/pages/file-upload/src/worker/canvasRenderer.js
+++ aula-01-m4demuxer-canvas-renderer/app/pages/file-upload/src/worker/canvasRenderer.js
@@ -2,41 +2,42 @@ let _canvas
 let _ctx
 export default class CanvasRenderer {
 
-    /** @param {VideoFrame} frame */
-    static draw(frame) {
-        const { displayHeight, displayWidth } = frame
+    /** #@param {VideoFrame} frame */
+    static draw (frame) {
+        const { displayHeight, displayWidth } = frame;
 
-        _canvas.width = displayWidth
-        _canvas.height = displayHeight
+        _canvas.width = displayWidth;
+        _canvas.height = displayHeight;
         _ctx.drawImage(
             frame,
             0,
             0,
             displayWidth,
             displayHeight
-        )
-        frame.close()
+        );
+
+        frame.close();
     }
 
+    /** @param {HTMLCanvasElement} canvas */
     static getRenderer(canvas) {
-        const renderer = this
-        let pendingFrame = null
-        _canvas = canvas
+        const renderer = this;
+        let pendingFrame = null;
+        _canvas = canvas;
         _ctx = canvas.getContext('2d')
 
         return frame => {
             const renderAnimationFrame = () => {
-                renderer.draw(pendingFrame)
-                pendingFrame = null
+                renderer.draw(pendingFrame);
+                pendingFrame = null;
             }
-
-            if (!pendingFrame) {
-                requestAnimationFrame(renderAnimationFrame)
+            if(!pendingFrame) {
+                requestAnimationFrame(renderAnimationFrame);
             } else {
-                pendingFrame.close()
+                pendingFrame.close();
             }
-
             pendingFrame = frame;
         }
+
     }
 }
