688b0e26579c8cc472c2b45fb769841743fa5cdb
Kelvin Modesto <km.modesto@gmail.com>
2023-10-27 00:15:17 -0300

    Aula 02

---
+++ aula-01-m4demuxer-canvas-renderer/app/pages/file-upload/src/worker/worker.js
@@ -0,0 +1,52 @@
+import MP4Demuxer from "./mp4Demuxer.js";
+import VideoProcessor from "./videoProcessor.js";
+import CanvasRenderer from "./canvasRenderer.js";
+
+const mp4Demuxer = new MP4Demuxer();
+const videoProcessor = new VideoProcessor({
+    mp4Demuxer
+});
+const qvgaConstraints = {
+    width: 320,
+    height: 240
+}
+
+const vgaConstraints = {
+    width: 640,
+    height: 480
+}
+
+const hdConstraints = {
+    width: 1280,
+    height: 720
+}
+
+const encoderConfig = {
+    ...qvgaConstraints,
+    bitrate: 10e6,
+    // webm
+    codec: 'vp09.00.10.08',
+    pt: 4,
+    hardwareAcceleration: 'prefer-software',
+    // mp4
+    // codec: 'avc1.42002A',
+    // pt: 1,
+    // hardwareAcceleration: 'prefer-hardware',
+    // avc: {
+    //     format: 'annexb'
+    // }
+}
+
+onmessage = async ({ data }) => {
+    const renderFrame = CanvasRenderer.getRenderer(data.canvas)
+    await videoProcessor.start({
+        file: data.file,
+        renderFrame,
+        encoderConfig,
+        sendMessage(message) {
+            self.postMessage(message)
+        }
+    })
+    self.postMessage({ status: 'done' });
+
+}
